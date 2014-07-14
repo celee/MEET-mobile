04c9abc5bd56701900bde0db0cc2296efecfccf1
celee <celee@mit.edu>
2014-07-14 12:29:57 -0400

    set up screens

--- GreenUp/main.py
+++ GreenUp/main.py
@@ -4,22 +4,6 @@ kivy.require('1.1.0')
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
-
-
-
-#from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-
-#from parse_rest.datatypes import Object
-#from parse_rest.connection import register
-
-
-
-
-
-
-
 class HomeScreen(Screen):
     pass
 
@@ -38,49 +22,28 @@ class LeaderBoardScreen(Screen):
 class WaterBoardScreen(Screen):
     pass
         
-class RecyclingBoardScreen(Screen):
+class RecyclingScreen(Screen):
     pass
         
-class ElectricityBoardScreen(Screen):
+class ElectricityScreen(Screen):
     pass
         
-class FuelBoardScreen(Screen):
+class FuelScreen(Screen):
     pass
 
-
-
-
-
-
-class MyWidget(Widget):
-    button1 = ObjectProperty(None)
-    button2 = ObjectProperty(None)
-    
-    def my_callback(instance, value, action, color):
-        if action == 1:
-            colorData = ColorButton(colorOfButton = color)
-            colorData.save()
-            print('Saved a new ColorButton with color %s.' % color)
-        elif action == 0:
-            colorData = ColorButton.Query.filter(colorOfButton = color)
-            if colorData.count() >= 1:
-                colorData = colorData.limit(1).get()
-                colorData.delete()
-                print('Deleted a ColorButton with color %s.' % color)
-            else:
-                print('No ColorButtons with color %s to delete.' % color)
-
-class ColorButton(Object):
-    pass
-    
 class GreenUpApp(App):
     def build(self):
-        #register('ns8i6BTFDzQHlx50ZDSXYhnaCLlyTyWnVTJyYkEm','UPXReuTMMmObBOSZ1k1cfggmoapENKOb2H9fxBDm')
-        
         # Create the screen manager
         sm = ScreenManager(transition=FadeTransition())
-        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(HomeScreen(name='home'))
         sm.add_widget(SettingsScreen(name='settings'))
+        sm.add_widget(ProfileScreen(name='profile'))
+        sm.add_widget(AchievementScreen(name='achievement'))
+        sm.add_widget(LeaderBoardScreen(name='leader'))
+        sm.add_widget(WaterScreen(name='water'))
+        sm.add_widget(RecyclingScreen(name='recycling'))
+        sm.add_widget(ElectricityScreen(name='electric'))
+        sm.add_widget(FuelScreen(name='fuel'))
         return sm
 
 if __name__ == '__main__':
