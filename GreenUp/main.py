ba19943c67aad65694d864d02582c8949dcea783
celee <celee@mit.edu>
2014-07-14 22:58:12 -0400

    set up Login and Home screen

--- GreenUp/main.py
+++ GreenUp/main.py
@@ -3,27 +3,30 @@ kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
 
 class LoginScreen(Screen):
     username = ObjectProperty(None)
     password = ObjectProperty(None)
-    error = ObjectProperty(None)
-
-    def button_clicked(instance):
-        if password.text == "hi":
-            self.manager.switch_to(HomeScreen(username=self.username))
+    error = ObjectProperty(None, rebind=True)
+    
+    def button_clicked(self):
+        if self.password.text == "hi":
+            self.manager.switch_to(HomeScreen(username=self.username.text))
         else:
-            error.text = "Username and password do not match. Try again."
+            self.error.text = "Login Failed. Try again."
+            self.error.color = [1,0,0,1]
 
 
 class HomeScreen(Screen):
-    username=''
-    userDisplayName=''
-
-    def __init__(self,username):
-        self.username = username
-        self.userDisplayName = username
+    username = ''
+    userDisplayName = ''
+    
+    def __init__(self, **kwargs):
+        # make sure we aren't overriding any important functionality
+        self.username = kwargs.get('username', "guest")
+        self.userDisplayName = kwargs.get('username', "guest")
+        super(HomeScreen, self).__init__(**kwargs)
 
 class SettingsScreen(Screen):
     pass
@@ -54,7 +57,7 @@ class GreenUpApp(App):
         # Create the screen manager
         sm = ScreenManager(transition=FadeTransition())
         sm.add_widget(LoginScreen(name='login'))
-        sm.add_widget(HomeScreen(name='home'))
+        sm.add_widget(HomeScreen(name='home',username='bob'))
         # sm.add_widget(SettingsScreen(name='settings'))
         # sm.add_widget(ProfileScreen(name='profile'))
         # sm.add_widget(AchievementScreen(name='achievement'))
