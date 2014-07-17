2eb20288a506b1602798f5f432f11fdad48d1096
celee <celee@mit.edu>
2014-07-17 12:54:13 -0400

    Added button icons and all the various screens

--- GreenUp/main.py
+++ GreenUp/main.py
@@ -8,6 +8,7 @@ from kivy.properties import ObjectProperty, StringProperty
 from parse_rest.datatypes import Object
 from parse_rest.connection import register
 from parse_rest.user import User
+from parse_rest.core import ParseError
 
 class LoginScreen(Screen):
     username = ObjectProperty(None)
@@ -15,69 +16,112 @@ class LoginScreen(Screen):
     error = ObjectProperty(None, rebind=True)
     
     def login_button(self):
-        u = User.login(self.username.text, self.password.text)
-
-        if u.is_authenticated():
-            self.manager.switch_to(HomeScreen(username=self.username.text))
-        else:
+        try:
+            u = User.login(self.username.text, self.password.text)
+            app = kivy.app.App.get_running_app()
+            app.user = u
+            if not self.manager.has_screen('home'):
+                self.manager.add_widget(HomeScreen(name='home'))
+            self.manager.current = 'home'
+        except ParseError:
             self.error.text = "Login Failed. Try again."
             self.error.color = [1,0,0,1]
 
     def signup_button(self):
-        u = User.signup(self.username.text, self.password.text)
-        self.error.text = "User created successfully. Please Login."
-        self.error.color = [0,1,0,1]
+        try:
+            u = User.signup(self.username.text, self.password.text)
+            u.displayName = u.username
+            u.save()
+            self.error.text = "User created successfully. Please Login."
+            self.error.color = [0,1,0,1]
+        except ParseError:
+            self.error.text = "Signup Failed. Choose another username."
+            self.error.color = [1,0,0,1]
 
 class HomeScreen(Screen):
-    username = ''
     userDisplayName = ''
     
     def __init__(self, **kwargs):
-        # make sure we aren't overriding any important functionality
-        self.username = kwargs.get('username', "guest")
-        self.userDisplayName = kwargs.get('username', "guest")
-        super(HomeScreen, self).__init__(**kwargs)
-
+        app = kivy.app.App.get_running_app()
+        u = app.user
+        if not u.is_authenticated():
+            self.manager.current = 'home'
+        else:
+            self.userDisplayName = u.displayName
+            # make sure we aren't overriding any important functionality
+            super(HomeScreen, self).__init__(**kwargs)
+            
+    def button_pressed(self, buttonText):
+        if buttonText == 'Settings':
+            if not self.manager.has_screen('settings'):
+                self.manager.add_widget(SettingsScreen(name='settings'))
+            self.manager.current = 'settings'
+        elif buttonText == 'Profile':
+            if not self.manager.has_screen('profile'):
+                self.manager.add_widget(ProfileScreen(name='profile'))
+            self.manager.current = 'profile'
+        elif buttonText == 'Achievements':
+            if not self.manager.has_screen('achievements'):
+                self.manager.add_widget(AchievementsScreen(name='achievements'))
+            self.manager.current = 'achievements'
+        elif buttonText == 'Water':
+            if not self.manager.has_screen('water'):
+                self.manager.add_widget(WaterScreen(name='water'))
+            self.manager.current = 'water'
+        elif buttonText == 'Recycling':
+            if not self.manager.has_screen('recycling'):
+                self.manager.add_widget(RecyclingScreen(name='recycling'))
+            self.manager.current = 'recycling'
+        elif buttonText == 'Electricity':
+            if not self.manager.has_screen('electricity'):
+                self.manager.add_widget(ElectricityScreen(name='electricity'))
+            self.manager.current = 'electricity'
+        elif buttonText == 'Fuel':
+            if not self.manager.has_screen('fuel'):
+                self.manager.add_widget(FuelScreen(name='fuel'))
+            self.manager.current = 'fuel'
+            
 class SettingsScreen(Screen):
-    pass
+    def go_home(self):
+        self.manager.current = 'home'
         
 class ProfileScreen(Screen):
-    pass
+    def go_home(self):
+        self.manager.current = 'home'
         
-class AchievementScreen(Screen):
-    pass
+class AchievementsScreen(Screen):
+    def go_home(self):
+        self.manager.current = 'home'
         
 class LeaderBoardScreen(Screen):
-    pass
+    def go_home(self):
+        self.manager.current = 'home'
         
-class WaterBoardScreen(Screen):
-    pass
+class WaterScreen(Screen):
+    def go_home(self):
+        self.manager.current = 'home'
         
 class RecyclingScreen(Screen):
-    pass
+    def go_home(self):
+        self.manager.current = 'home'
         
 class ElectricityScreen(Screen):
-    pass
+    def go_home(self):
+        self.manager.current = 'home'
         
 class FuelScreen(Screen):
-    pass
+    def go_home(self):
+        self.manager.current = 'home'
 
 class GreenUpApp(App):
+    user = None
+
     def build(self):
         register('jeV1sPLXL6cq8EsWAK0Zyoac8b97duQWyXYK7sfE','fl6hQ9dv94IbtA0I7kPzSrawL165HqupSjCFdo3D')
         
         # Create the screen manager
         sm = ScreenManager(transition=FadeTransition())
         sm.add_widget(LoginScreen(name='login'))
-        sm.add_widget(HomeScreen(name='home',username='bob'))
-        # sm.add_widget(SettingsScreen(name='settings'))
-        # sm.add_widget(ProfileScreen(name='profile'))
-        # sm.add_widget(AchievementScreen(name='achievement'))
-        # sm.add_widget(LeaderBoardScreen(name='leader'))
-        # sm.add_widget(WaterScreen(name='water'))
-        # sm.add_widget(RecyclingScreen(name='recycling'))
-        # sm.add_widget(ElectricityScreen(name='electric'))
-        # sm.add_widget(FuelScreen(name='fuel'))
         return sm
 
     def on_pause(self):
